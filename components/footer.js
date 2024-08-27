e44d332db50ce1ee2aef1495f8a7eb04ee7bab9d
parkhyeseong0326 <2017cococo@gmail.com>
2024-08-27 20:08:20 +0900

    8_27

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <div className="footer">
+            <h1>푸터임</h1>
+        </div>
+    )
+}
---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/layout";
+export default function PageNotFound(){
+    return(
+        <Layout>
+            <h1>페이지를 찾을 수 없습니다.</h1>
+        </Layout>
+    )
+}
+
